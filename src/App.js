4037be917be19e040ccefe25ca89c351b390ab26
holystorySeo <holystory.seo@gmail.com>
2022-03-16 15:45:24 +0900

    Fix SearchBar CSS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import SearchBar from './components/SearchBar';
 import OptionsItem from './components/OptionsItem';
 
 function App() {
   return (
     <>
+      <SearchBar />
       <OptionsItem name="테스트" emoji="🚀" />
       <OptionsItem name="테스트" emoji="📷" />
       <OptionsItem name="테스트" emoji="🧐" />
