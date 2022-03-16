b3cc602427d30a5a16944bb1ac7be1e47c5f4d8c
y-solb <ysolb99@gmail.com>
2022-03-16 17:00:10 +0900

    Feat: Merge components

--- src/components/List.js
+++ src/components/List.js
@@ -1,23 +1,43 @@
 import React from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 import Title from './Title';
 import EmojiMenus from '../data/EmojiMock';
 import OptionsItem from './OptionsItem';
 import Counter from './Counter';
 
-function List() {
+function List({ options }) {
   return (
     <ListcContainer>
       <Title />
       <ListBox>
-        {EmojiMenus.map((item) => {
-          return <OptionsItem name={item.name} emoji={item.emoji} />;
-        })}
+        {options
+          ? options.map((item) => {
+              return <OptionsItem name={item.name} emoji={item.emoji} />;
+            })
+          : null}
       </ListBox>
       <Counter total={EmojiMenus.length} />
     </ListcContainer>
   );
 }
+
+List.propTypes = {
+  options: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      topId: PropTypes.number,
+      code: PropTypes.string,
+      name: PropTypes.string,
+      nameEn: PropTypes.string,
+      nameKo: PropTypes.string,
+      route: PropTypes.string,
+      ordinal: PropTypes.number,
+      visible: PropTypes.bool,
+      emoji: PropTypes.string,
+    })
+  ).isRequired,
+};
 
 export default List;
 
