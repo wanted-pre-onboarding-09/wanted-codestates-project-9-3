9faff0e5c3b790a09d32540ed10868ffa819bb47
youngjeJO <yj01591@gmail.com>
2022-03-16 15:49:18 +0900

    feat list component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from 'react';
-import OptionsItem from './components/OptionsItem';
+
+import List from './components/List';
 
 function App() {
-  return (
-    <>
-      <OptionsItem name="테스트" emoji="🚀" />
-      <OptionsItem name="테스트" emoji="📷" />
-      <OptionsItem name="테스트" emoji="🧐" />
-      <OptionsItem name="테스트" emoji="🛎" />
-      <OptionsItem name="테스트" emoji="👩‍💻" />
-    </>
-  );
+  return <List />;
 }
 
 export default App;
