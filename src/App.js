b947c90de213e7245aaaa8edd538ec856164c07c
y-solb <ysolb99@gmail.com>
2022-03-16 20:28:37 +0900

    Remove: useSelector

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import styled from 'styled-components';
-import { useSelector } from 'react-redux';
 import SearchBar from './components/SearchBar';
 import List from './components/List';
 import ButtonTab from './components/ButtonTab';
